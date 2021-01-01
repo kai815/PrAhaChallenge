1f3c48234e54276d41c45a810f0e0c6be771ff5a
kai815 <h.kaizuka815@gmail.com>
2021-01-01 11:25:06 +0900

    課題1の実装

--- web-basis/request-parse-server/index.js
+++ web-basis/request-parse-server/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 8080;
+
+app.use(express.json())
 
 app.get('/', function(req, res){
-  res.send("hello world")
+  res.status(200);
+  res.send({"text":"hello world"});
 })
-app.listen(8080)
+
+app.post('/', function(req, res){
+  if(req.headers["content-type"]==="application/json"){
+    return res.status(201).send(req.body);
+  }
+  return res.status(400).send({ 
+    errorMessage: "Invalid content-type",
+    "content-type":req.headers["content-type"]
+  });
+})
+app.listen(port)
