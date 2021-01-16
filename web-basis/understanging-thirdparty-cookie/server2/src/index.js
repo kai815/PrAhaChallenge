00b48feadeaf771a42c371f8bef8c04952fffda9
kai815 <h.kaizuka815@gmail.com>
2021-01-16 19:42:24 +0900

    2台のサーバにするためパスの変更とサードパーティクッキーのセット

--- web-basis/understanging-thirdparty-cookie/server2/src/index.js
+++ web-basis/understanging-thirdparty-cookie/server2/src/index.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 
-
+//こっちは読み込まれる側の想定のサーバngrokにホストする
 app.use(express.static(__dirname + '/public',{
   setHeaders: function (res, path, stat) {
-    res.cookie("test", "cookie",{
-      httpOnly:true
-    })
-    res.cookie("hoge", "hoge",{
-      domain:"hoge.com",
-      httpOnly:true
-    })
+    //このcookieがサードパーティになる
     res.cookie("hoge2", "hoge2",{
-      domain:"hoge2.com",
       httpOnly:true
     })
+    
   }
 }))
 
