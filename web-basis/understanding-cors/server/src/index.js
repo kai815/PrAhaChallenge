a1044bcadb3933e454f38521de13bb042430c2c4
kai815 <h.kaizuka815@gmail.com>
2021-01-24 10:51:55 +0900

    課題3の実装

--- web-basis/understanding-cors/server/src/index.js
+++ web-basis/understanding-cors/server/src/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
-const app = express();
-const port = 8080;
+const htmlServer = express();
+const htmlServerPort = 8080;
 
-
-app.use(express.static(__dirname + '/public',{
+//HTMLファイルを返すだけのサーバ
+htmlServer.use(express.static(__dirname + '/public',{
 }))
+htmlServer.listen(htmlServerPort)
 
+//ここからcorsのサーバ
+const corsServer = express();
+const corsServerPort = 3000;
 
-app.listen(port)
+const allowCrossDomain = function(req, res, next) {
+  console.log("al")
+  //許すドメインやメソッドの定義
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Methods', 'POST,OPTIONS')
+
+  // プリフライト
+  if ('OPTIONS' === req.method) {
+    console.log("preflightが行われる");
+    res.send(200)
+  } else {
+    next()
+  }
+}
+corsServer.use(express.json());
+corsServer.use(express.urlencoded({ extended: true }));
+corsServer.use(allowCrossDomain);
+
+corsServer.post('/', function(req, res){
+  res.status(200);
+  res.send({text:"post success"});
+})
+
+corsServer.listen(corsServerPort)
