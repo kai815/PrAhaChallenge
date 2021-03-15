747d14883351d2393723860c72f5d06d4e559830
kai815 <h.kaizuka815@gmail.com>
2021-03-15 09:25:16 +0900

    スナップショットの分割

--- test/storybook/my-app/src/Storyshots.test.js
+++ test/storybook/my-app/src/Storyshots.test.js
@@ -1,2 +1,5 @@
-import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
+initStoryshots({
+  integrityOptions: { cwd: __dirname }, // it will start searching from the current directory
+  test: multiSnapshotWithOptions(),
+});
