fbc691a16d94aced03135a8a501903abc3932ed5
kai815 <h.kaizuka815@gmail.com>
2021-05-15 19:58:19 +0900

    storybookに引き分けの状態を追加

--- test/storybook/my-app/src/components/game/Game.stories.tsx
+++ test/storybook/my-app/src/components/game/Game.stories.tsx
@@ -22,3 +22,20 @@ GmaeAllTriangle.args = {
   history:[{squares:["△","△","△","△","△","△","△","△","△"]}],
   stepNumber:0
 };
+
+export const GmaeDrawResult = Template.bind({});
+GmaeDrawResult.args = {
+  history:[
+    {squares:["", "", "", "", "", "", "", "", ""]},
+    {squares:["X", "", "", "", "", "", "", "", ""]},
+    {squares:["X", "O", "", "", "", "", "", "", ""]},
+    {squares:["X", "O", "", "X", "", "", "", "", ""]},
+    {squares:["X", "O", "", "X", "", "", "O", "", ""]},
+    {squares:["X", "O", "", "X", "", "", "O", "X", ""]},
+    {squares:["X", "O", "", "X", "", "", "O", "X", "O"]},
+    {squares:["X", "O", "", "X", "", "X", "O", "X", "O"]},
+    {squares:["X", "O", "", "X", "O", "X", "O", "X", "O"]},
+    {squares:["X", "O", "X", "X", "O", "X", "O", "X", "O"]},
+  ],
+  stepNumber:9
+};
