83600708f64838129738334b2af295be30049467
kai815 <h.kaizuka815@gmail.com>
2022-03-05 14:53:53 +0900

    不要なファイルの削除

--- frontend/praha-challenge-front-app/pages/index.tsx
+++ frontend/praha-challenge-front-app/pages/index.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
-import Layout from '../components/Layout'
 import Text from '../components/atoms/Text/index'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
@@ -13,7 +12,7 @@ const IndexPage = () => (
     <Text as="h1" size="3">
       Hello, from Stitches.
     </Text>
-  </Layout>
+  </>
 )
 
 export default IndexPage
