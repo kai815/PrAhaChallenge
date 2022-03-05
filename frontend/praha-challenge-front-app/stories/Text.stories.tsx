f224053f6f6664d80ddd22fa347fbe2ffbb17259
kai815 <h.kaizuka815@gmail.com>
2022-03-05 10:31:34 +0900

    storyの追加

---
+++ frontend/praha-challenge-front-app/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Text from '../components/Text';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Text',
+  component: Text,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    size: {String},
+  },
+} as ComponentMeta<typeof Text>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Text> = ((args) => {return (<Text size={args.size}>テスト</Text>)});
+export const Default = Template.bind({});
+Default.args = {
+  size:"2"
+}
