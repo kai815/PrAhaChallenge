caa4db7a72c94a625345c3e816504969d1829ccb
kai815 <h.kaizuka815@gmail.com>
2022-03-19 12:28:51 +0900

    textとtagの位置をスタイル

--- frontend/praha-challenge-front-app/src/components/organisms/Card/Post/index.tsx
+++ frontend/praha-challenge-front-app/src/components/organisms/Card/Post/index.tsx
@@ -28,6 +28,11 @@ const CardOutLine = styled('div',{
   boxShadow: "0 0 #0000, 0 0 #0000, 0 0 #0000, 0 0 #0000, 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
   maxWidth: "56rem",
 })
+const LayoutDateTag = styled('div',{
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+})
 
 
 const PostCard = ({
@@ -39,8 +44,10 @@ const PostCard = ({
 }:PropsType)=>{
   return (
     <CardOutLine>
-      <Text as="span" color="grey600" size="base" weight="light">{date}</Text>
-      <LinkButton text={tag.text} href={tag.href}/>
+      <LayoutDateTag>
+        <Text as="span" color="grey600" size="base" weight="light">{date}</Text>
+        <LinkButton text={tag.text} href={tag.href}/>
+      </LayoutDateTag>
     </CardOutLine>
   )
 }
