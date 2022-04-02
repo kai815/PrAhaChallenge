eb541eace30fcabe3c7d6d867636c979007e836a
kai815 <h.kaizuka815@gmail.com>
2022-04-02 11:40:16 +0900

    課題2

--- frontend/praha-challenge-front-app/src/components/atoms/Button/Button.stories.tsx
+++ frontend/praha-challenge-front-app/src/components/atoms/Button/Button.stories.tsx
@@ -32,3 +32,36 @@ Default.args = {
   disabled: false,
   onClick: log
 };
+
+const doApply = () => {
+  console.log("応募する")
+}
+
+const ApplyTemplate: ComponentStory<typeof Button> = (args) => {
+  return <Button {...args} >応募する</Button>;
+};
+
+export const Apply = ApplyTemplate.bind({});
+Apply.args = {
+  color: 'blue',
+  size: 'medium',
+  disabled: false,
+  onClick: doApply
+};
+
+
+const doDelete = () => {
+  console.log("削除する")
+}
+
+const DeleteTemplate: ComponentStory<typeof Button> = (args) => {
+  return <Button {...args} >削除する</Button>;
+};
+
+export const Delete = DeleteTemplate.bind({});
+Delete.args = {
+  color: 'red',
+  size: 'small',
+  disabled: false,
+  onClick: doDelete
+};
